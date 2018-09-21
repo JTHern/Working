628f2b7fa6c3a298d118c69f624d350377fc30c0
Jason <31118660+JTHern@users.noreply.github.com>
2018-09-20 20:37:26 -0500

    Update RouterTab.py

--- RouterTab.py
+++ RouterTab.py
@@ -1,24 +1,117 @@
-import serial.tools.list_ports
-ports = list(serial.tools.list_ports.comports())
-for p in ports:
-    print p
-
 import settings
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QCheckBox, QHBoxLayout, QGridLayout, QGroupBox, QLabel, QLineEdit, QMessageBox,
-                             QPlainTextEdit, QPushButton, QWidget)
+import serial.tools.list_ports
+import re
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtWidgets import (QCheckBox, QHBoxLayout, QGridLayout, QGroupBox, QLabel, QLineEdit, QPlainTextEdit,
+                             QPushButton, QWidget)
 from message_handler import LoggingMessageHandler
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import NetMikoTimeoutException, NetMikoAuthenticationException
 
 
+class Thread(QThread):
+    signal = pyqtSignal('PyQt_PyObject')
+    # This Class threads the connection so we don't freeze the entire program waiting on the connection to take place.
+
+    def __init__(self):
+        QThread.__init__(self)
+        self.device = {}
+
+    @staticmethod
+    def get_version_cisco(show_ver):
+        match = re.search(r"1.*", show_ver)
+        return float(match.group(0))
+
+    # run method gets called when we start() the thread
+    def run(self):
+        if self.device['device_type'] == 'cisco_ios_serial':
+            try:
+                console = ConnectHandler(**self.device)  # Connect to the Device to verify credentials.
+                console.enable()  # Enter Enable Mode
+                show_ver = console.send_command('show run | inc version 1')
+                self.signal.emit(show_ver)
+                version = self.get_version_cisco(show_ver)
+                if version >= 15.4:
+                    pass
+                else:
+                    show_flash = console.send_command('dir flash: | i .bin')
+                    self.signal.emit('ERROR: Old version of IOS Detected\n Correct version may be in flash:')
+                    self.signal.emit(show_flash)
+                    self.signal.emit('==================================\n'
+                                     'Ensure the config you plan to load is compatible\n'
+                                     ' with the version of IOS you are using.\n '
+                                     'If not contact the help desk.\n'
+                                     '==================================')
+                console.disconnect()  # disconnect so it can be modified later on the LoadTab
+                self.signal.emit('Credentials Verified on Console!')  # Congrats you made it
+            except ValueError:  # most likely com port fault.
+                self.signal.emit('...COM Port does not appear to be working. \nTry one of these:')
+                ports = list(serial.tools.list_ports.comports())  # You used the wrong one here let me help you.
+                for p in ports:  # may just automate this in the future using this method if only one is found
+                    self.signal.emit(p[0])
+                return
+            except NetMikoAuthenticationException:  # Exactly what it says in the error.
+                self.signal.emit("Check your username/password. Make sure you have an account on this device.")
+                return
+        elif self.device['device_type'] == 'cisco_ios_telnet':
+            try:
+                telnet = ConnectHandler(**self.device)  # Connect to the Device to verify credentials.
+                telnet.enable()  # Enter Enable Mode
+                show_ver = telnet.send_command('show run | inc version 1')
+                self.signal.emit(show_ver)
+                version = self.get_version_cisco(show_ver)
+                if version >= 15.4:
+                    pass
+                else:
+                    show_flash = telnet.send_command('dir flash: | i .bin')
+                    self.signal.emit('Version below 15.4, correct version may be in flash:')
+                    self.signal.emit(show_flash)
+                    self.signal.emit('===============\n'
+                                     'Ensure the config you plan to load is compatible with'
+                                     ' the version of IOS you are using. If not contact the help desk.')
+                telnet.disconnect()
+                self.signal.emit('Credentials Verified on Telnet!')
+            except TimeoutError:  # Exactly what it says in the error.
+                self.signal.emit("Telnet Error: Make sure the IP address is correct.")
+                return
+            except NetMikoAuthenticationException:  # Exactly what it says in the error.
+                self.signal.emit("Check your username/password. Make sure you have an account on this device.")
+                return
+        elif self.device['device_type'] == 'cisco_ios':
+            try:
+                ssh = ConnectHandler(**self.device)  # Connect to the Device to verify credentials.
+                ssh.enable()  # Enter Enable Mode
+                show_ver = ssh.send_command('show run | inc version 1')
+                self.signal.emit(show_ver)
+                version = self.get_version_cisco(show_ver)
+                if version >= 15.4:
+                    pass
+                else:
+                    show_flash = ssh.send_command('dir flash: | i .bin')
+                    self.signal.emit('Version below 15.4, correct version may be in flash:')
+                    self.signal.emit(show_flash)
+                    self.signal.emit('===============\n'
+                                     'Ensure the config you plan to load is compatible with'
+                                     ' the version of IOS you are using. If not contact the help desk.')
+                ssh.disconnect()
+                self.signal.emit('Credentials Verified on SSH!')
+            except NetMikoTimeoutException:  # Exactly what it says in the error.
+                self.signal.emit("SSH Error: Make sure the IP address is correct.")
+                return
+            except NetMikoAuthenticationException:  # Exactly what it says in the error.
+                self.signal.emit("Check your username/password. Make sure you have an account on this device.")
+                return
+        else:
+            return
+
+
 class RouterInfo(QWidget):
     label = "RouterInfo"
 
-    def __init__(self):
+    def __init__(self, parent=None):
         """ Initialise the page. """
 
-        super().__init__()
+        super().__init__(parent)
         '''sets up a grid layout for the tab'''
         layout = QGridLayout()  # Page will use a grid layout.
 
@@ -27,19 +120,16 @@ class RouterInfo(QWidget):
         con_method_sel = QGroupBox("Connection method")
         con_method_sel_layout = QHBoxLayout()  # Lays out the below buttons horizontally.
 
-        self._console_button = QCheckBox("Console",
-                                         whatsThis="console",
-                                         checked=False, stateChanged=self._console_button)
+        self._console_button = QCheckBox("Console", checked=False, stateChanged=self._console_button)
+        self._console_button.setToolTip("If connecting over Serial - COM port needed.")
         con_method_sel_layout.addWidget(self._console_button)
 
-        self._telnet_button = QCheckBox("Telnet",
-                                        whatsThis="telnet",
-                                        checked=False, stateChanged=self._telnet_button)
+        self._telnet_button = QCheckBox("Telnet", checked=False, stateChanged=self._telnet_button)
+        self._telnet_button.setToolTip("If connecting over Telnet. - IP needed")
         con_method_sel_layout.addWidget(self._telnet_button)
 
-        self._ssh_button = QCheckBox("SSH",
-                                     whatsThis="ssh",
-                                     checked=False, stateChanged=self._ssh_button)
+        self._ssh_button = QCheckBox("SSH", checked=False, stateChanged=self._ssh_button)
+        self._ssh_button.setToolTip("If connecting over SSH. - IP needed")
         con_method_sel_layout.addWidget(self._ssh_button)
 
         con_method_sel.setLayout(con_method_sel_layout)
@@ -56,27 +146,28 @@ class RouterInfo(QWidget):
         layout.addWidget(label2, 2, 0)
         self.password = QLineEdit()
         self.password.setEchoMode(QLineEdit.Password)  # turn off echo mode for password
+
         layout.addWidget(self.password, 2, 1)
 
         '''The ip field and label'''
         label3 = QLabel('     COM Port or IP')
         layout.addWidget(label3, 3, 0)
         self.ip = QLineEdit()
+        self.ip.setToolTip("Com Port = COM1 or IP = 192.168.0.1")
         layout.addWidget(self.ip, 3, 1)
 
         '''The verify pushbutton'''
-        verify = QPushButton("Verify",
-                             whatsThis="Select IOS configuration to load.",
-                             clicked=self.verify
-                             )
-        layout.addWidget(verify, 3, 2)
+        self.verify_button = QPushButton("Verify", clicked=self._verify)
+        self.verify_button.setToolTip("Verify - Connects using provided credentials.")
+        layout.addWidget(self.verify_button, 3, 2)
+        self.verify_thread = Thread()
+        self.verify_thread.signal.connect(self.finished)
 
-        label0 = QLabel('Once credentials are set click Verify.')
+        label0 = QLabel('Once credentials are set click Verify.') # label at bottom of screen
         layout.addWidget(label0, 4, 1)
 
         '''The output screen'''
-        self._log_viewer = QPlainTextEdit(
-            whatsThis="This displays the messages generated when loading the router.", readOnly=True)
+        self._log_viewer = QPlainTextEdit(whatsThis="This displays the messages generated.", readOnly=True)
         layout.addWidget(self._log_viewer, 5, 1, 1, 1)
 
         '''Displays the layout'''
@@ -84,86 +175,58 @@ class RouterInfo(QWidget):
 
     '''These fields allow for actions to take place based on the above input.'''
 
-    def verify(self):
-        """ Verifies the credentials supplied button. """
+    def _verify(self):  # We want to verify the information in a new thread so we don't freeze up the entire app.
         logger = LoggingMessageHandler(bool(), self._log_viewer)
-        if self.con_method == 'cisco_ios_serial':
-            if 'COM' not in self.ip.text():
-                logger = LoggingMessageHandler(bool(), self._log_viewer)
-                logger.clear()
-                logger.status_message("Com Port field requires COM1 or COM2 or COM3 etc...")
-                return
-            if self.ip.text() == '' or self.username.text() == '' or self.password.text() == '':
-                logger = LoggingMessageHandler(bool(), self._log_viewer)
-                logger.clear()
-                logger.status_message("All Fields must be Completed.")
-                return
-            device = {
-                'device_type': self.con_method,
-                'username': self.username.text(),
-                'password': self.password.text(),
-                'serial_settings': {'port': self.ip.text()}
-            }
-            settings.device.append(device)
-            try:
-                console = ConnectHandler(**device)  # Connect to the Device to verify credentials.
-                console.disconnect()
-                logger.status_message('Credentials Verified on Console!')
-            except ValueError:
-                logger.status_message("Console is not working. Make sure you have connectivity.")
-            except NetMikoAuthenticationException:
-                logger.status_message("Check your username/password. Make sure you have an account on this device.")
-            pass
-
-        elif self.con_method == 'cisco_ios_telnet':
-            if self.ip.text() == '' or self.username.text() == '' or self.password.text() == '':
-                logger = LoggingMessageHandler(bool(), self._log_viewer)
-                logger.clear()
-                logger.status_message("All Fields must be Completed.")
-                return
-            device = {
-                'device_type': self.con_method,
-                'ip': self.ip.text(),
-                'username': self.username.text(),
-                'password': self.password.text()
-            }
-            settings.device.append(device)
-            try:
-                telnet = ConnectHandler(**device)  # Connect to the Device to verify credentials.
-                telnet.disconnect()
-                logger.status_message('Credentials Verified on Telnet!')
-            except TimeoutError:
-                logger.status_message("Telnet Error: Make sure the IP address is correct.")
-            except NetMikoAuthenticationException:
-                logger.status_message("Check your username/password. Make sure you have an account on this device.")
-            pass
-
-        elif self.con_method == 'cisco_ios':
-            if self.ip.text() == '' or self.username.text() == '' or self.password.text() == '':
-                logger = LoggingMessageHandler(bool(), self._log_viewer)
-                logger.clear()
-                logger.status_message("All Fields must be Completed.")
-                return
-            device = {
-                'device_type': self.con_method,
-                'ip': self.ip.text(),
-                'username': self.username.text(),
-                'password': self.password.text()
-            }
-            settings.device.append(device)
-            try:
-                ssh = ConnectHandler(**device)  # Connect to the Device to verify credentials.
-                ssh.disconnect()
-                logger.status_message('Credentials Verified on SSH!')
-            except NetMikoTimeoutException:
-                logger.status_message("SSH Error: Make sure the IP address is correct.")
-            except NetMikoAuthenticationException:
-                logger.status_message("Check your username/password. Make sure you have an account on this device.")
-            pass
-        else:
+        if self.ip.text() == '' or self.username.text() == '' or self.password.text() == '':
             logger.clear()
             logger.status_message("All Fields must be Completed.")
             return
+        if self.con_method == 'cisco_ios_serial':
+            if 'COM' not in self.ip.text():
+                logger.clear()
+                logger.status_message("Com Port field requires COM1 or COM2 or COM3 etc...")
+                return
+            device = {
+                'device_type': self.con_method,
+                'global_delay_factor': 2,
+                'username': self.username.text(),
+                'password': self.password.text(),
+                'serial_settings': {
+                    'port': self.ip.text()}
+            }
+            settings.device.append(device)
+            self.verify_thread.device = device
+        elif self.con_method == 'cisco_ios_telnet':
+            device = {
+                'device_type': self.con_method,
+                'ip': self.ip.text(),
+                'username': self.username.text(),
+                'password': self.password.text()
+            }
+            settings.device.append(device)
+            self.verify_thread.device = device
+        elif self.con_method == 'cisco_ios':
+            device = {
+                'device_type': self.con_method,
+                'ip': self.ip.text(),
+                'username': self.username.text(),
+                'password': self.password.text()
+            }
+            settings.device.append(device)
+            self.verify_thread.device = device
+        else:
+            logger.clear()
+            logger.status_message("Please select a connection method.")
+            return
+
+        self.verify_button.setEnabled(False)  # Disables the pushButton
+        logger.status_message("Verifying... (This may take a while on Console)")
+        self.verify_thread.start()
+
+    def finished(self, result):
+        logger = LoggingMessageHandler(bool(), self._log_viewer)
+        logger.status_message(result)
+        self.verify_button.setEnabled(True)  # Enable the pushButton
 
     def _console_button(self, state):
         """ if console is checked uncheck the others """
